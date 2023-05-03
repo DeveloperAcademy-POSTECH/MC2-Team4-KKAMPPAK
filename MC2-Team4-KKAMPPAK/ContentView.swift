52a0d58ec74a45cc8718535bdec7633d3f98347a
chamroro <chamsae@handong.ac.kr>
2023-05-03 12:31:46 +0900

    initial commit

---
+++ MC2-Team4-KKAMPPAK/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MC2-Team4-KKAMPPAK
+//
+//  Created by 김하은 on 2023/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("좋아요")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MC2-Team4-KKAMPPAK/MC2_Team4_KKAMPPAKApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2_Team4_KKAMPPAKApp.swift
+//  MC2-Team4-KKAMPPAK
+//
+//  Created by 김하은 on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct MC2_Team4_KKAMPPAKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
