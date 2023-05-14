24e4d578419dae2963a8d538ce5d49b701415d58
whaeundo <1916388@sookmyung.ac.kr>
2023-05-14 17:35:43 +0900

    [Chore/#23] Modify progress bar color

--- MC2-Team4-KKAMPPAK/View/Component/ProgressBar.swift
+++ MC2-Team4-KKAMPPAK/View/Component/ProgressBar.swift
@@ -14,11 +14,10 @@ struct ProgressBar: View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
                 Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
-                    .opacity(0.3)
-                    .foregroundColor(Color(UIColor.systemTeal))
+                    .foregroundColor(Color("secondCircle"))
                 
                 Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
-                    .foregroundColor(Color(UIColor.systemBlue))
+                    .foregroundColor(Color("centerCircle"))
                     .animation(.linear)
             }.cornerRadius(45.0)
         }
